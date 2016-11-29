361406a295d3e737f43938f5396b0b651bd0b14e
Garance Cordonnier <garancecordonnier@gmail.com>
2016-11-29 09:39:08 +0100

    Add 2 pay buttons for coffee and regular consos, and an Escadron class.

--- main.py
+++ main.py
@@ -2,26 +2,60 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.properties import StringProperty, NumericProperty, BooleanProperty
+from kivy.properties import StringProperty, NumericProperty, BooleanProperty, ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
 
 class Bar(Widget):
+
     pass
 
-class Conso(Widget):
-    prix = NumericProperty(0)
-    def setPrice(self,px):
+class Conso:
+
+    def __init__(self, px):
         self.prix = px
 
 class Personnel(Widget):
+
     grade = StringProperty("")
-    name = StringProperty("Cordonnier")
+    first_name = StringProperty("")
+    last_name = StringProperty("")
     solde = NumericProperty(0)
-    admin = BooleanProperty(False)
 
     def addMoney(self, money):
         self.solde += money
 
+    def payCons(self, cons):
+        self.solde -= cons.prix
+
+    def eatConso(self):
+        #Pour mettre plusieurs contenus dans une popup, utiliser une BoxLayout.
+        box = BoxLayout()
+        buttcaf = Button(text = 'Cafe')
+        buttcons = Button(text = 'Conso')
+        box.add_widget(buttcaf)
+        box.add_widget(buttcons)
+        popup = Popup(title = "Choisis ta conso",
+                    size_hint = (0.2,0.2),
+                    content = box)
+        def action(cons):
+            self.payCons(cons)
+            popup.dismiss()
+        #Pour une raison quelconque la touche cafe paye les deux consos, d'ou le 0.3
+        buttcons.bind(on_touch_down = lambda x,y : action(Conso(0.8)))
+        buttcaf.bind(on_touch_down = lambda x,y : action(Conso(-0.3)))
+        popup.open()
+
+class Escadron:
+
+    def __init__(self):
+        self.pers = []
+
+    def addPerso(self,pers):
+        self.pers.append(pers)
+
 class BarApp(App):
+
     def build(self):
         return Bar()
 
