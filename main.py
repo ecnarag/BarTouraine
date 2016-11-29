bd08493b2a4ccde158e853df7d8410d2bed5262b
Garance Cordonnier <garancecordonnier@gmail.com>
2016-11-29 14:32:40 +0100

    Add a button to add money to a Personnel instance.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.properties import StringProperty, NumericProperty, BooleanProperty, ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
 class Bar(Widget):
 
@@ -17,9 +18,9 @@ class Conso:
 
 class Personnel(Widget):
 
-    grade = StringProperty("")
-    first_name = StringProperty("")
-    last_name = StringProperty("")
+    grade = StringProperty("ASP")
+    first_name = StringProperty("Garance")
+    last_name = StringProperty("Cordonnier")
     solde = NumericProperty(0)
 
     def addMoney(self, money):
@@ -28,25 +29,44 @@ class Personnel(Widget):
     def payCons(self, cons):
         self.solde -= cons.prix
 
-    def eatConso(self):
+    def eatConso(self): #Fonction qui s'ouvre quand on clique sur son icone.
         #Pour mettre plusieurs contenus dans une popup, utiliser une BoxLayout.
-        box = BoxLayout()
-        buttcaf = Button(text = 'Cafe')
-        buttcons = Button(text = 'Conso')
+
+        box = BoxLayout(orientation = 'horizontal', spacing = 10)
+        buttcaf = Button(text = 'Cafe', size_hint = (0.3,1))
+        buttcons = Button(text = 'Conso', size_hint = (0.3,1))
+        buttadd = Button(text = 'Add mny', size_hint = (0.3,1))
         box.add_widget(buttcaf)
         box.add_widget(buttcons)
+        box.add_widget(buttadd)
+
         popup = Popup(title = "Choisis ta conso",
                     size_hint = (0.2,0.2),
                     content = box)
-        def action(cons):
+
+        def action(cons): #Action des boutons buttcaf et buttcons: consommation et fermeture de la popup
             self.payCons(cons)
             popup.dismiss()
-        #Pour une raison quelconque la touche cafe paye les deux consos, d'ou le 0.3
-        buttcons.bind(on_touch_down = lambda x,y : action(Conso(0.8)))
-        buttcaf.bind(on_touch_down = lambda x,y : action(Conso(-0.3)))
+
+        def actionask(): #Action du bouton buttadd : fermeture de popup et ouverture de popup2 avec un input utilisateur...
+            popup.dismiss()
+            textinput = TextInput(multiline = False,
+                                    focus = True,
+                                    text = "0")
+            popup2 = Popup(title = "Montant",
+                            size_hint = (0.2,0.2),
+                            content = textinput)
+            textinput.bind(on_text_validate = popup2.dismiss)
+            popup2.bind(on_dismiss = lambda x : self.addMoney(float(textinput.text)))
+            popup2.open()
+            return textinput.text
+
+        buttadd.bind(on_press = lambda x : actionask())
+        buttcons.bind(on_press = lambda x : action(Conso(0.8)))
+        buttcaf.bind(on_press = lambda x : action(Conso(0.5)))
         popup.open()
 
-class Escadron:
+class Escadron: #Pour permettre des trucs un peu globaux et surtout ajouter/retirer du personnel. (a completer pour le moment)
 
     def __init__(self):
         self.pers = []
