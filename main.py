e73f896c79b1df493b6f44a40a19094a5767e82a
Garance Cordonnier <garancecordonnier@gmail.com>
2016-12-05 17:24:15 +0100

    Add add/remove buttons + add colors

--- main.py
+++ main.py
@@ -22,8 +22,15 @@ from kivy.core.window import Window
 Cette application a été développée par Garance Cordonnier, X2016, pour le bar du Touraine.
 Il faut installer le module python KIVY (pip install kivy), le code est écrit pour Python 2 (oui, je sais, c'est sale,mais python3 ne fonctionnait pas avec kivy).
 Conseils pour le code : utilisez un gestionnaire de versions (j'ai utilisé GIT, niquel pour Linux et MacOS, existe aussi sous Windows) pour enregistrer vos modifications et garder un historique des versions précédentes, la doc kivy est en ligne à https://kivy.org/docs/ avec beaucoup d'exemples, beaucoup de réponses à vos questions sont sur le forum StackOverflow, la doc du module pickle est en ligne à https://docs.python.org/2/library/pickle.html et la plupart des modules écrits pour python ont leur doc sur https://anaconda.org.
-J'ai essayé de commenter au maximum (un peu en franglais ...) et d'avoir des conventions assez réglo : saut de ligne après les définitions des classes et entre deux blocs, noms des fonctions avec des majusculesAuDébutDeChaqueMot, noms de variables explicites, donc ça devrait être compréhensible. Bonne chance !"""
+J'ai essayé de commenter au maximum (un peu en franglais ...) et d'avoir des conventions assez réglo : saut de ligne après les définitions des classes et entre deux blocs, noms des fonctions avec des majusculesAuDébutDeChaqueMot, noms de variables explicites, donc ça devrait être compréhensible. N'hésitez pas à commenter au maximum et à faire le code le plus propre possible (factorisation de code et jolies conventions). Bonne chance !"""
 
+"""Ordre des classes: Carte - Conso - Stats - Escadron - Personnel - BarApp.
+La Carte contient les données générales sur les consos et la sérialization.
+La Conso contient les attributs d'une conso (nom, prix, total d'utilisation).
+Les Stats contiennent l'inventaire, les plus gros consommateurs, les plus mauvais payeurs, le total du bar et la fonction de remise à zéro.
+L'Escadron contient la sérialisation et les fonctions générales sur les utilisateurs.
+La classe Personnel contient les attributs d'un consommateur (nom, solde, nombre de consos ...) et les fonctions de consommation.
+La classe BarApp est la classe "principale" qui contient les fonctions de démarrage et de fermeture de l'application."""
 
 class Carte(GridLayout): #L'équivalent de Escadron -> Personnel pour les Consos
 
@@ -32,19 +39,19 @@ class Carte(GridLayout): #L'équivalent de Escadron -> Personnel pour les Consos
     cons_widg = {}
     n = 0
 
-    def saveCarte(self, fle):
+    def saveCarte(self, fle): #Enregistrer la carte dans un fichier
        pickle.dump({"n":self.n, "cons":self.cons},fle)
 
-    def loadCarte(self,fle):
+    def loadCarte(self,fle): #Télécharger la carte depuis un fichier
         x = pickle.load(fle)
         self.n = x["n"]
         self.cons = x["cons"]
 
-    def addConso(self,conso):
+    def addConso(self,conso): #Ajouter un type de conso à la carte (inutilisé)
         n += 1
         cons[conso.name] = {"px":conso.prix, "tot":conso.total}
 
-    def grid(self, perso):
+    def grid(self, perso): #Créer la grille de choix
         self.g = BoxLayout(orientation = 'horizontal')
         for i in self.cons.iteritems():
             butt = Conso(perso, i[1]["px"],i[0],i[1]["tot"], size_hint = (1.0/self.n, 1))
@@ -85,7 +92,7 @@ class Stats(BoxLayout):
         return sum([k["solde"] for k in self.escadron.pers])
 
     def inventaire(self):
-        result = "\n Consommations :\n"
+        result = "\nConsommations :\n"
         for k in self.carte.cons.iteritems():
             result += k[0] + " : " + str(k[1]["tot"]) + "\n"
         return result
@@ -109,16 +116,46 @@ class Escadron(GridLayout):
 
     #Initialisation
     g = ObjectProperty(GridLayout())
-    n = 4
+    n = 0
     pers = []
     pers_widg = []
 
     def addPerso(self,perso): #Add someone to the Escadron
         self.n += 1
-        self.pers.append(perso.dic)
+        self.pers.append({"lst":perso.last_name, "solde":perso.solde, "cns": perso.consos})
+        self.pers_widg.append(perso)
+
+    def removePerso(self, name):
+        self.pers = list(filter(lambda x : x["lst"] != name.text, self.pers))
+        self.pers_widg = list(filter(lambda x : x.last_name != name.text, self.pers_widg))
+        self.n = len(self.pers)
+
+    def removeButton(self):
+        name = TextInput(multiline = False,
+                        focus = True,
+                        text = "")
+        popup = Popup(title = "Supprimer un personnel",
+                    size_hint = (0.2,0.2),
+                    content = name)
+        name.bind(on_text_validate = popup.dismiss)
+        popup.bind(on_dismiss = lambda x : self.removePerso(name))
+        popup.open()
+        return name.text
+
+    def addButton(self):
+        name = TextInput(multiline = False,
+                    focus = True,
+                    text = "")
+        popup = Popup(title = "Nouveau personnel",
+                        size_hint = (0.2,0.2),
+                        content = name)
+        name.bind(on_text_validate = popup.dismiss)
+        popup.bind(on_dismiss = lambda x: self.addPerso(Personnel(lst = name.text)))
+        popup.open()
+        return name.text
 
     def saveEsc(self,fle): #Save Escadron in a file
-        for i in range(len(self.pers_widg)): #Conversion Widgets -> Dicts to be pickable/unpickable
+        for i in range(self.n): #Conversion Widgets -> Dicts to be pickable/unpickable
             k = self.pers_widg[i]
             self.pers[i] = {"lst":k.last_name, "solde":k.solde, "cns":k.consos} #Update values to make sure any changes are recorded
             #(We probably use the saveEsc function when closing the app)
@@ -131,9 +168,10 @@ class Escadron(GridLayout):
 
     def grid(self): #Build grid
         self.g = GridLayout(cols = int(math.sqrt(self.n)))
+        color = [(0,0,1,0.5),(0,0,1,0.6)]
         for i in range(len(self.pers)): #Add everyone in the grid
             k = self.pers[i]
-            perso = Personnel(lst = k["lst"], sld = k["solde"], cns = k["cns"])
+            perso = Personnel(lst = k["lst"], sld = k["solde"], cns = k["cns"], background_color = color[(i%2)])
             self.pers_widg.append(perso)
             self.g.add_widget(self.pers_widg[i])
         return self.g
@@ -159,7 +197,7 @@ class Personnel(Button):
         self.solde -= cons.prix
         self.consos += 1
 
-    def multipleCons(self,n,cons):
+    def multipleCons(self,n,cons): #Payer plusieurs consos
         for k in range(n):
             self.payCons(cons)
 
@@ -190,10 +228,12 @@ class Personnel(Button):
     def click(self): #Fonction appelée quand on clique sur son bouton
         carte = app.card.grid(self)
         content = BoxLayout(orientation = 'vertical', spacing = 10)
+        if abs(self.solde) < 10**(-6): #Avant que j'ajoute ce test, les erreurs de calcul de python mettaient un "1.8E-14" au lieu de 0 (par exemple)
+            self.solde = 0
         content.add_widget(Label(text = "Vous avez " + str(self.solde) + "€"))
         content.add_widget(carte)
         content.add_widget(Button(text = "Add\nmoney", on_press = lambda x : self.actionask()))
-        popup = Popup(title = "Choisis ta conso",
+        popup = Popup(title = "Choisis ta conso, mon cher " + self.last_name,
                         size_hint = (0.3,0.5),
                         content = content)
         popup.open()
@@ -211,16 +251,19 @@ class BarApp(App):
         self.root = root = self.esc.grid() #Construire une grille
         self.card = Carte()
         self.card.loadCarte(open("cartefile.txt","rb"))
+        root.add_widget(Label()) #Une case vide pour séparer les noms du reste -- si on pouvait ajouter depuis la fin ça serait mieux
+        #mais j'ai cru comprendre dans la doc de kivy que c'était impossible ...
         stat = Stats(self.esc, self.card) #Construire les statistiques
-        root.add_widget(Label(text = "BIENVENUE"))
-        root.add_widget(Label(text = "AU BAR DU"))
-        root.add_widget(Label(text = "TOURAINE !"))
         root.add_widget(stat)
+        box = BoxLayout(orientation = "horizontal")
+        box.add_widget(Button(text = "Add", on_press = lambda x: self.esc.addButton()))
+        box.add_widget(Button(text = "Remove", on_press = lambda x : self.esc.removeButton()))
+        root.add_widget(box)
         return root
 
     def on_stop(self): #Ce que l'application fait quand on la ferme (sauvegarde des données)
-       self.esc.saveEsc(open("escadronfile.txt","wb")) #Escadronfile est le fichier dans lequel toutes les données sont écrites (en binaire)
-       self.card.saveCarte(open("cartefile.txt","wb"))
+        self.esc.saveEsc(open("escadronfile.txt","wb")) #Escadronfile est le fichier dans lequel toutes les données sont écrites (en binaire)
+        self.card.saveCarte(open("cartefile.txt","wb"))
 
 if __name__ == '__main__':
     BarApp().run()
@@ -236,10 +279,13 @@ Ce qui fonctionne :
 - les données sont sauvegardées quand l'app est fermée
 - il y a un bouton statistiques
 - l'appli est transférée sous android !!!
+- gestion des stocks
+- remise à zéro des stocks
+- affichage en rouge des personnes en négatif
+- ajouter/supprimer quelqu'un
+- couleurs alternées
 Ce qui ne fonctionne pas / ce qui reste à faire :
-- le dernier bouton a un comportement bizarre quand on l'utilise (le nom et le "€" disparaissent mais le solde est bien actualisé)
+- important ? actualiser les nouvelles personnes
 - faire autre chose que planter quand il y a un nombre négatif entré dans "add money"
 - l'affichage est pas du tout optimisé pour mon téléphone, il faut voir ce que ça donne sur tablette
-- la partie "Médailles" est inexistante
-- gérer les stocks ": fichier consofile.py, plusieurs boutons avec une liste comme escadron
-- il reste toute la déco à gérer """
+"""
